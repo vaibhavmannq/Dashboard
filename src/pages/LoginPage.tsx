a0070af23c1407bcc30d5cff2fe3c9b71d4aa3b5
vaibhavmannq <vaibhavmann.10@gmail.com>
2024-08-08 16:41:20 +0530

    first commit

---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,56 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { useRef } from "react";
+import { Link } from "react-router-dom";
+ 
+const LoginPage = () => {
+  const emailRef = useRef<HTMLInputElement>(null);  
+  const passwordRef = useRef<HTMLInputElement>(null);
+  
+  const handleLoginSubmit = () => {
+    const email = emailRef.current?.value;
+    const password = passwordRef.current?.value;
+    console.log("data", {email, password});
+
+    // make server call
+  };
+
+  return ( 
+  <section className='flex justify-center items-center h-screen'>
+    <Card className="w-full max-w-sm">
+  <CardHeader>
+    <CardTitle className="text-2xl">Login</CardTitle>
+    <CardDescription>
+      Enter your email below to login to your account.
+    </CardDescription>
+  </CardHeader>
+  <CardContent className="grid gap-4">
+    <div className="grid gap-2">
+      <Label htmlFor="email">Email</Label>
+      <Input ref={emailRef} id="email" type="email" placeholder="m@example.com" required />
+    </div>
+    <div className="grid gap-2">
+      <Label htmlFor="password">Password</Label>
+      <Input ref={passwordRef} id="password" type="password" required />
+    </div>
+  </CardContent>
+  <CardFooter>
+    <div className="w-full">
+    <Button onClick={handleLoginSubmit} className="w-full">Sign in</Button>
+
+    <div className="mt-4 text-center text-sm">
+      Don't have an account?{" "}
+      <Link to={'/auth/register'} className="underline">
+        Sign up
+      </Link>
+    </div>
+    </div>
+  </CardFooter>
+</Card>
+  </section>
+)};
+
+export default LoginPage;
+
---
+++ src/router.tsx
@@ -0,0 +1,41 @@
+import { createBrowserRouter } from "react-router-dom";
+import LoginPage from "@/pages/LoginPage";
+import HomePage from "@/pages/HomePage";
+import RegisterPage from "./pages/RegisterPage";
+import DashboardLayout from "./layouts/DashboardLayout";
+import AnalyticsPage from "./pages/AnalyticsPage";
+import AuthLayout from "./layouts/AuthLayout";
+
+const Router = createBrowserRouter([
+  {
+    path: 'dashboard',
+    element: <DashboardLayout />,
+    children: [
+      { 
+        path: "home", 
+        element: <HomePage />,
+      },
+      { 
+        path: "analytics", 
+        element: <AnalyticsPage />,
+      }
+    ]
+  },
+  {
+    path: '/auth',
+    element: <AuthLayout />,
+    children: [
+      { 
+        path: "login", 
+        element: <LoginPage />,
+      },
+      { 
+        path: "register", 
+        element: <RegisterPage />,
+      },
+    ],   
+  },
+]);
+
+
+export default Router;
