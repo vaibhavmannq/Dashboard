e92b7cf6602f99bfe2dbeecc7a087d6d7ae4f26c
vaibhavmannq <vaibhavmann.10@gmail.com>
2024-09-07 17:48:17 +0530

    add loading state

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -3,7 +3,9 @@ import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle }
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { login } from "@/http/api";
+import { cn } from "@/lib/utils";
 import { useMutation } from "@tanstack/react-query";
+import { LoaderCircle } from "lucide-react";
 import { useRef } from "react";
 import { Link, useNavigate } from "react-router-dom";
  
@@ -40,6 +42,7 @@ const LoginPage = () => {
     <CardTitle className="text-2xl">Login</CardTitle>
     <CardDescription>
       Enter your email below to login to your account.
+      {mutation.isPending && <div>Loading...</div>}
     </CardDescription>
   </CardHeader>
   <CardContent className="grid gap-4">
@@ -54,7 +57,11 @@ const LoginPage = () => {
   </CardContent>
   <CardFooter>
     <div className="w-full">
-    <Button onClick={handleLoginSubmit} className="w-full">Sign in</Button>
+    <Button onClick={handleLoginSubmit} className="w-full" disabled={mutation.isPending}>
+      {
+        mutation.isPending && (<LoaderCircle className="animate-spin" />)
+      }
+      <span className="ml-2">Sign in</span></Button>
 
     <div className="mt-4 text-center text-sm">
       Don't have an account?{" "}
