c2ff4800cddac9caa92068c74a175b0433af3353
vaibhavmannq <vaibhavmann.10@gmail.com>
2024-09-29 15:29:24 +0530

    add dark mode

--- src/components/ui/theme-provider.tsx
+++ src/components/ui/theme-provider.tsx
@@ -71,5 +71,3 @@ export const useTheme = () => {
 
   return context
 }
-
-export default ThemeProvider;
--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -1,3 +1,45 @@
+// import useTokenStore from '@/store';
+// import { Navigate, Outlet } from 'react-router-dom';
+
+// const AuthLayout = () => {
+//     const token = useTokenStore((state) => state.token);
+
+//     if (token) {
+//         return <Navigate to={'/dashboard/home'} replace />;
+//     }
+
+//     return (
+//         <>
+//             <Outlet />
+//         </>
+//     );
+// };
+
+// export default AuthLayout;
+
+// import { ThemeProvider } from '@/components/ui/theme-provider';
+// import useTokenStore from '@/store';
+// import { Navigate, Outlet } from 'react-router-dom';
+// // import { ThemeProvider } from '@/components/theme-provider';
+
+// const AuthLayout = () => {
+//     const token = useTokenStore((state) => state.token);
+
+//     if (token) {
+//         return <Navigate to={'/dashboard/home'} replace />;
+//     }
+
+//     return (
+//         <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+//             <Outlet />
+//         </ThemeProvider>
+//     );
+// };
+
+// export default AuthLayout;
+
+import { ModeToggle } from '@/components/ui/mode-toggle';
+import { ThemeProvider } from '@/components/ui/theme-provider';
 import useTokenStore from '@/store';
 import { Navigate, Outlet } from 'react-router-dom';
 
@@ -9,9 +51,12 @@ const AuthLayout = () => {
     }
 
     return (
-        <>
+        <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+            <div className="flex justify-end p-4">
+                <ModeToggle /> {/* Add the ModeToggle component */}
+            </div>
             <Outlet />
-        </>
+        </ThemeProvider>
     );
 };
 
